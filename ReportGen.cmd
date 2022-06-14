905dbda7596de52f4edd32d7c1a8257ef17b7a3b
Noble Wilson <92472549+CueM6@users.noreply.github.com>
2022-06-14 15:30:36 -0500

    initial commit

---
+++ ReportGen.cmd
@@ -0,0 +1,18 @@
+@echo off
+>LifeTrends_NWERR-%DATE:~-4%-%DATE:~4,2%-%DATE:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.log (
+echo Checking your system info, Please wating...
+
+net user
+
+ipconfig /all | find "Physical Address" 
+
+ipconfig | find "IPv4" 
+ipconfig | find "Default Gateway"
+ipconfig | find "Subnet Mask"
+
+systeminfo
+tasklist
+
+)
+
+@pause
