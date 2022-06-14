4b271faedb9b0c4b6bbd5ff159eec0440078fd64
Noble Wilson <92472549+CueM6@users.noreply.github.com>
2022-06-14 17:04:42 -0500

    Update ReportGen.cmd

--- ReportGen.cmd
+++ ReportGen.cmd
@@ -1,6 +1,6 @@
 @echo off
 >LifeTrends_NWERR-%DATE:~-4%-%DATE:~4,2%-%DATE:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.log (
-echo Checking your system info, Please wating...
+echo Checking your system info, Please wait...
 
 net user
 
